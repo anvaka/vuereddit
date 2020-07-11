f017f7932b993b85fb17e583e1e16a7c88fca51d
Andriy Kashcha <anvaka@gmail.com>
2020-07-11 12:42:20 -0700

    corrected description

--- src/components/Subreddit.vue
+++ src/components/Subreddit.vue
@@ -49,7 +49,7 @@ export default {
   computed: {
     description() {
       const {about} = this;
-      return (about && about.description) || '';
+      return (about && about.public_description) || '';
     },
     canShowPosts() {
       if (this.loading) return false;
